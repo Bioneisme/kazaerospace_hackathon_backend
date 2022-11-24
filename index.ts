0e524a8b1d288ec2e5bdfa914fd5b43e1db7c300
Bioneisme <bioneisme@gmail.com>
2022-11-24 22:48:15 +0600

    Initial commit

---
+++ index.ts
@@ -0,0 +1,25 @@
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import {SERVER_PORT} from "./config/settings";
+import {config} from "./config/mikro-orm";
+import logger from "./config/logger";
+import express, {Application} from "express";
+import {EntityManager, MikroORM} from "@mikro-orm/core";
+
+const app: Application = express();
+
+export const DI = {} as {
+    orm: MikroORM,
+    em: EntityManager
+};
+
+app.use(express.json());
+app.use(cors());
+app.use(cookieParser());
+
+app.listen(SERVER_PORT, async () => {
+    DI.orm = await MikroORM.init(config);
+    DI.em = DI.orm.em.fork();
+
+    logger.info(`Server Started on port ${SERVER_PORT}`);
+});
