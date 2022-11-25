98e0945e279832b756ccaee54dbd3cf936e643c8
Bioneisme <bioneisme@gmail.com>
2022-11-25 12:47:33 +0600

    Added product type and categories enum

--- types.d.ts
+++ types.d.ts
@@ -2,3 +2,24 @@ import {Request} from "express";
 import {Users} from "./entities";
 
 export type UserRequest = Request & {user: Users | undefined};
+
+export type productObject = {
+    link: string,
+    category: string,
+    title: string,
+    store: string,
+    price: number,
+    img: string | undefined
+}
+
+export enum technoDomCategories {
+    'laptops'= "noutbuki-i-komp-jutery/noutbuki-i-aksessuary/noutbuki",
+    'phones'="smartfony-i-gadzhety/smartfony-i-telefony/smartfony",
+    'gadgets'="smartfony-i-gadzhety/gadzhety",
+    'tablets'="smartfony-i-gadzhety/planshety-i-jelektronnye-knigi/planshety",
+    'televisions'="tv-audio-foto-video/televizory",
+    'computers'="noutbuki-i-komp-jutery/komp-jutery-i-monitory/stacionarnye-pk",
+    'audio'="tv-audio-foto-video/audio-tehnika/akustika",
+    'cameras'="fototehnika-i-kvadrokoptery/jekshn-kamery-i-aksessuary",
+    'digital'="tv-audio-foto-video/cifrovoe-sputnikovoe-tv"
+}
