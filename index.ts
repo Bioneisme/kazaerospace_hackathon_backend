31b67b3acfbf1ba7133ba0cff42190fe212159a6
Bioneisme <bioneisme@gmail.com>
2022-11-25 19:32:06 +0600

    Stop at SIGINT and SIGTERM processes

--- index.ts
+++ index.ts
@@ -4,10 +4,12 @@ import userRoutes from "./routes/userRoute";
 import productRoute from "./routes/productRoute";
 import {CLIENT_URL, SERVER_PORT} from "./config/settings";
 import {config} from "./config/mikro-orm";
+import {parserCron} from "./helpers/cron";
 import logger from "./config/logger";
 import cookieParser from "cookie-parser";
 import express, {Application} from "express";
 import {EntityManager, MikroORM} from "@mikro-orm/core";
+import {generalParser} from "./helpers/parsers/generalParser";
 
 const app: Application = express();
 
@@ -32,6 +34,18 @@ app.use(logging);
 app.listen(SERVER_PORT, async () => {
     DI.orm = await MikroORM.init(config);
     DI.em = DI.orm.em.fork();
+    parserCron.start();
 
     logger.info(`Server Started on port ${SERVER_PORT}`);
+});
+
+
+
+process.once('SIGINT', () => {
+    parserCron.stop();
+    DI.orm.close().then(() => logger.info(`PgSQL was closed by SIGINT process`));
+});
+process.once('SIGTERM', () => {
+    parserCron.stop();
+    DI.orm.close().then(() => logger.info(`PgSQL was closed by SIGTERM process`));
 });
